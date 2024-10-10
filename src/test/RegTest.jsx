b01801f8c8a469edda8e6de4cda237dc023fcd57
baekmerve <baekmerve@gmail.com>
2024-10-10 16:47:06 +0900

    username duplicate

--- src/test/RegTest.jsx
+++ src/test/RegTest.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "../pages/Login/Form.css";
 import {
+  checkUsername,
   registerUser,
   sendVerificationCode,
   verifyCode,
@@ -18,7 +19,6 @@ const RegTest = () => {
   });
 
   const [isCodeSent, setIsCodeSent] = useState(false);
-
   const [loading, setLoading] = useState(false);
   const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
@@ -26,6 +26,7 @@ const RegTest = () => {
   const [verificationCodeError, setVerificationCodeError] = useState("");
   const [isVerified, setIsVerified] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [isUsernameAvailable, setIsUsernameAvailable] = useState(false);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -41,48 +42,38 @@ const RegTest = () => {
   } = useSelector((state) => state.auth);
 
   const validateForm = () => {
-    let isValid = true;
-
-    // Reset errors
-    setUsernameError("");
-    setPasswordError("");
-    setEmailError("");
-    setVerificationCodeError("");
+    let errors = {};
 
     // Username validation
     if (!formData.username) {
-      setUsernameError("Username is required.");
-      isValid = false;
+      errors.username = "Username is required.";
     } else if (formData.username.length < 6) {
-      setUsernameError("Username must be at least 6 characters.");
-      isValid = false;
+      console.log(errors)
+      errors.username = "Username must be at least 6 characters.";
+    } else if (!isUsernameAvailable) {
+      errors.username = "Username is already taken.";
     }
 
     // Password validation
     if (!formData.password) {
-      setPasswordError("Password is required.");
-      isValid = false;
+      errors.password = "Password is required.";
     } else if (formData.password.length < 8) {
-      setPasswordError("Password must be at least 8 characters.");
-      isValid = false;
+      errors.password = "Password must be at least 8 characters.";
     }
 
     // Email validation
     if (!formData.email) {
-      setEmailError("Email is required.");
-      isValid = false;
+      errors.email = "Email is required.";
     } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      setEmailError("Email is not valid.");
-      isValid = false;
+      errors.email = "Email is not valid.";
     }
 
-    // Verification code validation (if applicable)
+    // Verification code validation
     if (isCodeSent && !formData.verificationCode) {
-      setVerificationCodeError("Verification code is required.");
-      isValid = false;
+      errors.verificationCode = "Verification code is required.";
     }
 
-    return isValid;
+    return errors;
   };
 
   const handleChange = (e) => {
@@ -92,22 +83,51 @@ const RegTest = () => {
     });
   };
 
+  const handleBlur = (e) => {
+    const errors = validateForm();
+    if (e.target.name === "username") setUsernameError(errors.username || "");
+    if (e.target.name === "password") setPasswordError(errors.password || "");
+    if (e.target.name === "email") setEmailError(errors.email || "");
+    if (e.target.name === "verificationCode")
+      setVerificationCodeError(errors.verificationCode || "");
+  };
+
+  const handleCheckUsername = async () => {
+    try {
+      const response = await dispatch(
+        checkUsername(formData.username)
+      ).unwrap();
+      if (response.message === "Username is available.") {
+        setUsernameError(""); // Clear error if available
+        setIsUsernameAvailable(true); // Set username as available
+      }
+    } catch (error) {
+      setUsernameError(error); // Set error message if taken
+      setIsUsernameAvailable(false); // Set username as not available
+    }
+  };
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!validateForm()) {
-      setLoading(false);
-      return; // Prevent submission if validation fails
-    }
+
+    const errors = validateForm();
+    setUsernameError(errors.username || "");
+    setPasswordError(errors.password || "");
+    setEmailError(errors.email || "");
+    setVerificationCodeError(errors.verificationCode || "");
+
+    if (Object.keys(errors).length > 0) return;
     try {
-      const resultAction = await dispatch(registerUser(formData)).unwrap();
+      const result = await dispatch(registerUser(formData)).unwrap();
       alert("Registered successfully!"); // Alert on successful registration
       // Reset form fields
       setFormData({
         username: "",
         email: "",
         password: "",
+        verificationCode: "",
       });
-
       // Navigate to the login page
       navigate("/api/user/login");
     } catch (error) {
@@ -126,13 +146,10 @@ const RegTest = () => {
   const handleSendCode = async (e) => {
     e.preventDefault();
 
-    if (!validateForm()) {
-      return; // Prevent sending code if validation fails
-    }
+    const errors = validateForm();
+    setEmailError(errors.email || "");
 
-    // if (!formData.username || !formData.email || !formData.password) {
-    //   return; // Add validation as needed
-    // }
+    if (Object.keys(errors).length > 0) return;
 
     try {
       await dispatch(sendVerificationCode(formData)).unwrap();
@@ -148,17 +165,10 @@ const RegTest = () => {
     e.preventDefault();
     setLoading(true);
 
-    // Check for validation errors
-    if (!validateForm()) {
-      setLoading(false);
-      return; // Prevent verification if validation fails
-    }
+    const errors = validateForm();
+    setVerificationCodeError(errors.verificationCode || "");
 
-    // Check for validation errors
-    // if (usernameError || passwordError || verificationCodeError) {
-    //   setLoading(false);
-    //   return;
-    // }
+    if (Object.keys(errors).length > 0) return;
 
     try {
       const response = await dispatch(verifyCode(formData)).unwrap(); // Unwrap to get the response directly
@@ -169,10 +179,12 @@ const RegTest = () => {
       } else {
         setIsVerified(false);
         setErrorMessage("Invalid verification code. Please try again.");
+        alert(response);
       }
     } catch (error) {
       setErrorMessage(error); // Set error message from thunk
-      alert(error);
+      console.log(error);
+      alert("Invalid verification code. Please try again.");
     } finally {
       setLoading(false);
       setVerificationCodeError("");
@@ -186,35 +198,41 @@ const RegTest = () => {
         id="userRegisterForm"
         className="auth-form"
         onSubmit={isCodeSent ? handleVerifyCode : handleSendCode}
-      >
-        <div>
-          <input
-            ref={usernameRef}
-            type="text"
-            name="username"
-            placeholder="Enter your username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-          <div className="error-text">
-            {usernameError && <p style={{ color: "red" }}>{usernameError}</p>}
-          </div>
+      ><div className="verify-box">
+        <input
+          ref={usernameRef}
+          type="text"
+          name="username"
+          placeholder="Enter your username"
+          value={formData.username}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          // required
+        />
+        <button
+          className="send-code-button"
+          type="button"
+          onClick={handleCheckUsername}
+          style={{ marginLeft: "10px" }}
+        >
+          Check Availability
+        </button>
+        </div>
+        <div className="error-text">
+          {usernameError && <p style={{ color: "red" }}>{usernameError}</p>}
         </div>
 
-        <div>
-          <input
-            ref={passwordRef}
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <div className="error-text">
-            {passwordError && <p style={{ color: "red" }}>{passwordError}</p>}
-          </div>
+        <input
+          ref={passwordRef}
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          value={formData.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        <div className="error-text">
+          {passwordError && <p style={{ color: "red" }}>{passwordError}</p>}
         </div>
 
         <div className="verify-box">
@@ -225,13 +243,18 @@ const RegTest = () => {
             placeholder="Enter your email"
             value={formData.email}
             onChange={handleChange}
-            required
+            onBlur={handleBlur}
           />
           <button
             className="send-code-button"
             type="submit"
             disabled={loading || isCodeSent}
-            style={{ marginLeft: "10px" }}
+            style={{
+              marginLeft: "10px",
+              backgroundColor: isCodeSent ? "#d3d3d3" : "tomato", // Gray out button
+              cursor: isCodeSent ? "not-allowed" : "pointer",
+              color: isCodeSent ? "#666" : "#fff",
+            }}
           >
             이메일 인증
           </button>
@@ -250,7 +273,7 @@ const RegTest = () => {
               placeholder="Enter verification code"
               value={formData.verificationCode}
               onChange={handleChange}
-              required
+              onBlur={handleBlur}
             />
             <div className="error-text">
               {verificationCodeError && (
