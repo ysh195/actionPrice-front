11b2d182a5094181033297062b81dc0eda22021f
baekmerve <baekmerve@gmail.com>
2024-11-05 12:31:12 +0900

    test4

--- src/components/MyPage/Favorites.jsx
+++ src/components/MyPage/Favorites.jsx
@@ -1,40 +1,60 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { Button, List, ListItem, ListItemText, Typography } from "@mui/material";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchFavoriteList } from "../../redux/slices/favoriteSlice";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Favorites = ({ username }) => {
   const dispatch = useDispatch();
-  const {favoriteList, status, error} = useSelector((state) => state.favorite);
+    const navigate = useNavigate();
+  const { favoriteList, status, error } = useSelector(
+    (state) => state.favorite
+  );
 
   useEffect(() => {
     dispatch(fetchFavoriteList(username));
   }, [dispatch, username]);
 
-   if (status === "loading")
-     return <Typography>Loading wishlist...</Typography>;
-   if (status === "failed") return <Typography>Error: {error}</Typography>;
+   const handleFavoriteClick = (url) => {
+     navigate(url);
+   };
 
-
-
+  if (status === "loading") return <Typography>Loading wishlist...</Typography>;
+  if (status === "failed") return <Typography>Error: {error}</Typography>;
 
   return (
     <div>
       <Typography>찜한 상품 목록</Typography>
-      <List>
+      {/* <List>
         {favoriteList.map((item) => (
-          <ListItem key={item.favoriteId}>
-            <ListItemText
-              primary={item.favoriteName}
-              // secondary={item.favoriteURL}
-            />
-            <Link to={item.favoriteURL}> {item.favoriteName} </Link>
+          <ListItem
+            key={item.favoriteId}
+            button
+            onClick={() => handleFavoriteClick(item.favoriteURL)}
+          >
+            {item.favoriteName}
           </ListItem>
         ))}
-      </List>
+      </List> */}
+
+      <div>
+
+        <ul>
+          {favoriteList.map((favorite) => (
+            <li key={favorite.favoriteId}>
+              <Link to={favorite.favoriteURL}>{favorite.favoriteName}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
